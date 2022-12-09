7d06c70b5ba91911605d6dd14af774f0c1620cf2
Hasanul-Banna <hbdu006@gmail.com>
2022-12-09 09:28:41 +0600

    gallery

--- index.js
+++ index.js
@@ -25,11 +25,47 @@ async function run() {
     try {
         await client.connect();
         const collection = client.db("PhotohouseBD").collection("redirect_links");
+        const MemberCollection = client.db("PhotohouseBD").collection("members");
+        const MagazinesCollection = client.db("PhotohouseBD").collection("magazines");
+        const galleryCollection = client.db("PhotohouseBD").collection("gallery");
 
+        app.get("/gallery", async (req, res) => {
+            const result = await galleryCollection.find({}).toArray();
+            res.send(result);
+        });
+        app.post('/gallery', (req, res) => {
+            galleryCollection.insertOne(req.body).then(response => res.send({ isSuccess: true, message: 'gallery image is successfully added!' }))
+        })
         app.get("/redirect_links", async (req, res) => {
             const result = await collection.find({}).toArray();
             res.send(result[0]);
         });
+        app.get("/members", async (req, res) => {
+            const result = await MemberCollection.find({}).toArray();
+            res.send(result);
+        });
+        app.post('/regn_memeber', (req, res) => {
+            const file = req.files.file;
+            const newImg = file.data;
+            const encImg = newImg.toString('base64');
+            var image = { contentType: file.mimetype, size: file.size, img: Buffer.from(encImg, 'base64') };
+
+            MemberCollection.insertOne({ ...req.body, image }).then(response => res.send({ isSuccess: true, message: 'Member is successfully registered!' }))
+        })
+        app.get("/magazines", async (req, res) => {
+            const result = await MagazinesCollection.find({}).toArray();
+            res.send(result);
+        });
+        app.post('/add_magazine', (req, res) => {
+            const body = {
+                name: 'Forbes',
+                image: 'https://i.ibb.co/YDVp6fP/M2.png',
+                redirect_link: "https://ibb.co/album/Bwcg28",
+                category: 'Newest'
+            }
+
+            MagazinesCollection.insertOne(body).then(response => res.send({ isSuccess: true, message: 'magazine is successfully added!' }))
+        })
         console.log("Connected successfully to server");
     } finally {
         // Ensures that the client will close when you finish/error
