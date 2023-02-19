768c5bf408d23e99c3139c42ed7ba9153b68ada8
Hasanul-Banna <hbdu006@gmail.com>
2023-02-19 17:38:00 +0600

    new_gallery

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fileUpload = require('express-fileupload');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectID } = require('mongodb');
 const nodemailer = require('nodemailer');
@@ -10,14 +12,27 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('./uploads'))
 app.use(cors());
-app.use(fileUpload());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
-
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { cb(null, 'uploads/'); },
+    filename: function (req, file, cb) { cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); },
+});
+const upload = multer({ storage: storage });
+app.post('/upload', upload.single('image'), (req, res) => {
+    try {
+        // const filePath = req.file.path;
+        const fileName = req.file.filename;
+        res.status(200).json({ message: 'File uploaded successfully', url: fileName });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to upload file' });
+    }
+});
 const port = 5000;
 const uri = "mongodb+srv://Photohouse:X4rfPrPyy0wHWjkX@cluster0.h9calhb.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -27,7 +42,7 @@ async function run() {
         const collection = client.db("PhotohouseBD").collection("redirect_links");
         const MemberCollection = client.db("PhotohouseBD").collection("members");
         const MagazinesCollection = client.db("PhotohouseBD").collection("magazines");
-        const galleryCollection = client.db("PhotohouseBD").collection("gallery");
+        const galleryCollection = client.db("PhotohouseBD").collection("gallery_collection");
         const homeSliderCollection = client.db("PhotohouseBD").collection("home_slider_images");
 
         app.get("/home_slider_images", async (req, res) => {
@@ -45,10 +60,6 @@ async function run() {
             const result = await collection.find({}).toArray();
             res.send(result[0]);
         });
-        app.get("/members", async (req, res) => {
-            const result = await MemberCollection.find({}).toArray();
-            res.send(result);
-        });
         app.post('/regn_memeber', (req, res) => {
             const file = req.files.file;
             const newImg = file.data;
@@ -61,16 +72,6 @@ async function run() {
             const result = await MagazinesCollection.find({}).toArray();
             res.send(result);
         });
-        app.post('/add_magazine', (req, res) => {
-            const body = {
-                name: 'Forbes',
-                image: 'https://i.ibb.co/YDVp6fP/M2.png',
-                redirect_link: "https://ibb.co/album/Bwcg28",
-                category: 'Newest'
-            }
-
-            MagazinesCollection.insertOne(body).then(response => res.send({ isSuccess: true, message: 'magazine is successfully added!' }))
-        })
         console.log("Connected successfully to server");
     } finally {
         // Ensures that the client will close when you finish/error
